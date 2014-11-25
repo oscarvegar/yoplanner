475b92d35506d1c48b755a239166cb132d860d0e
oscar.vega <oscar.vega@conceptomovil.com>
2014-11-24 20:58:23 -0600

    búsqueda con sort compra

--- api/controllers/RecintoController.js
+++ api/controllers/RecintoController.js
@@ -7,7 +7,7 @@
 //https://api.despegar.com/v3/hotels/availabilities?site=MX&checkin_date=2014-10-25&checkout_date=2014-10-30&destination=982&distribution=1&language=es&sorting=stars_descending&stars=3%2C4%2C5&pagesize=8
 ///availability/cities/{id}/hotels
 module.exports = {
-	findByCiudadId: function(req,res){
+    findByCiudadId: function(req,res){
         var hotelesVendidos = {PCM:[353356,352782]};
         var resSize = 11;
         var id = req.allParams().id;
@@ -19,14 +19,14 @@ module.exports = {
         // Start the request
         HttpClientService.httpsGET(options,function(response){
             try{ 
-               
+
                 var data = JSON.parse(response);
                 var ids = "";
                 var p = 0;
                 if(req.param('p')){
                     p = req.param('p');
                     resSize = 12;
-                }else{
+                }else if(hotelesVendidos[id]){
                     if(hotelesVendidos[id].length>0){
                         for(var i in hotelesVendidos[id]){
                             ids += hotelesVendidos[id][i]+",";
@@ -40,7 +40,7 @@ module.exports = {
                         continue;
                     }
                     ids += data.availability[d].hotel.id+",";
-                    
+
                 }
                 console.log(ids)
                 options = {
@@ -48,31 +48,34 @@ module.exports = {
                     path : "/hotels/"+ids+"?includeamenities=true&includesummary=true",
                     headers : {"X-ApiKey":"53df4ffd-5adb-48ce-9738-72cea4a5da30MX"},
                 };
-                 HttpClientService.httpsGET(options,function(resp){
-                     var hoteles = JSON.parse(resp);
-                     var arrResHot = [];
-                     console.log(hotelesVendidos[id]);
-                     for(var i=0;i<hotelesVendidos[id].length;i++){
-                         
-                         for(var j in hoteles.hotels){
-                             console.log(hoteles.hotels[j].id)
-                             console.log(hotelesVendidos[id][i])
-                             console.log("----------------------")
-                             if(hoteles.hotels[j].id==hotelesVendidos[id][i]){
-                                 var hot = hoteles.hotels[j];
-                                 hoteles.hotels.splice(j,1);
-                                 arrResHot.push(hot);
-                                 break;
-                             }
-                         }
-                     }
-                     hoteles.hotels = arrResHot.concat(hoteles.hotels);
-                     res.json(hoteles);
-                 });
+                HttpClientService.httpsGET(options,function(resp){
+                    var hoteles = JSON.parse(resp);
+                    var arrResHot = [];
+                    console.log(hotelesVendidos[id]);
+                    if(hotelesVendidos[id]){
+                        for(var i=0;i<hotelesVendidos[id].length;i++){
+
+                            for(var j in hoteles.hotels){
+                                console.log(hoteles.hotels[j].id)
+                                console.log(hotelesVendidos[id][i])
+                                console.log("----------------------")
+                                if(hoteles.hotels[j].id==hotelesVendidos[id][i]){
+                                    var hot = hoteles.hotels[j];
+                                    hoteles.hotels.splice(j,1);
+                                    arrResHot.push(hot);
+                                    break;
+                                }
+                            }
+                        }
+                        hoteles.hotels = arrResHot.concat(hoteles.hotels);
+                    }
+                    res.json(hoteles);
+                });
             }catch(ex){
+                console.log(ex);
                 res.json(500,{})   
             }
-           
+
         });
     }
 };
