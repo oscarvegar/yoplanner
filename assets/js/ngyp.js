db426eea0f4878faa9c7bcf53d5c2d379b2b6160
Eduardo Chico <eduardo.chico@gmail.com>
2014-12-09 20:01:12 -0600

    Cambios de estilos y widgets de google

--- assets/js/ngyp.js
+++ assets/js/ngyp.js
@@ -73,8 +73,222 @@ angular.module('yoPlannerApp', ['autocomplete','angular-flexslider','yp-index','
 
 
     $scope.selectResult = function(hotel) {
+		$scope.currentHotel = hotel;
+		
+		window.setTimeout(function() {
+			//set your google maps parameters
+			var latitude = $scope.currentHotel.geoLocation.latitude,
+				longitude = $scope.currentHotel.geoLocation.longitude,
+				map_zoom = 14;
+
+			//google map custom marker icon - .png fallback for IE11
+			var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+			var marker_url = ( is_internetExplorer11 ) ? 'img/cd-icon-location.png' : 'img/cd-icon-location.svg';
+				
+			//define the basic color of your map, plus a value for saturation and brightness
+			var	main_color = '#2d313f',
+				saturation_value= -20,
+				brightness_value= 5;
+
+			//we define here the style of the map
+			var style= [ 
+				{
+					//set saturation for the labels on the map
+					elementType: "labels",
+					stylers: [
+						{saturation: saturation_value}
+					]
+				},  
+				{	//poi stands for point of interest - don't show these lables on the map 
+					featureType: "poi",
+					elementType: "labels",
+					stylers: [
+						{visibility: "off"}
+					]
+				},
+				{
+					//don't show highways lables on the map
+					featureType: 'road.highway',
+					elementType: 'labels',
+					stylers: [
+						{visibility: "off"}
+					]
+				}, 
+				{ 	
+					//don't show local road lables on the map
+					featureType: "road.local", 
+					elementType: "labels.icon", 
+					stylers: [
+						{visibility: "off"} 
+					] 
+				},
+				{ 
+					//don't show arterial road lables on the map
+					featureType: "road.arterial", 
+					elementType: "labels.icon", 
+					stylers: [
+						{visibility: "off"}
+					] 
+				},
+				{
+					//don't show road lables on the map
+					featureType: "road",
+					elementType: "geometry.stroke",
+					stylers: [
+						{visibility: "off"}
+					]
+				}, 
+				//style different elements on the map
+				{ 
+					featureType: "transit", 
+					elementType: "geometry.fill", 
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				}, 
+				{
+					featureType: "poi",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "poi.government",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "poi.sport_complex",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "poi.attraction",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "poi.business",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "transit",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "transit.station",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "landscape",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+					
+				},
+				{
+					featureType: "road",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				},
+				{
+					featureType: "road.highway",
+					elementType: "geometry.fill",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				}, 
+				{
+					featureType: "water",
+					elementType: "geometry",
+					stylers: [
+						{ hue: main_color },
+						{ visibility: "on" }, 
+						{ lightness: brightness_value }, 
+						{ saturation: saturation_value }
+					]
+				}
+			];
+				
+			//set google map options
+			var map_options = {
+				center: new google.maps.LatLng(latitude, longitude),
+				zoom: map_zoom,
+				panControl: false,
+				zoomControl: false,
+				mapTypeControl: false,
+				streetViewControl: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				scrollwheel: false,
+				styles: style,
+			}
+			//inizialize the map
+			var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+			//add a custom marker to the map				
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(latitude, longitude),
+				map: map,
+				visible: true,
+				icon: marker_url,
+			});
+		
+		}, 1000);
+		
+		gapi.post.go();
+
+	
         
-        $scope.currentHotel = hotel;
         $scope.currentHotelMap= $sce.trustAsResourceUrl("https://www.google.com/maps/embed/v1/place?key=AIzaSyBwFDofYVj2wDbbrdZl1_Bossxi-_hdlhU&q="+$scope.currentHotel.geoLocation.latitude+","+$scope.currentHotel.geoLocation.longitude);
         $scope.hideResults = false;
         $scope.showDetail = true;
