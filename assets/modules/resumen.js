7001cb079dc8591fe057966fc6f3dd961139f9b4
oscarvegar <oscarvegar@gmail.com>
2015-01-06 22:18:45 -0600

    cambios a index y resumen

--- assets/modules/resumen.js
+++ assets/modules/resumen.js
@@ -17,15 +17,38 @@ angular.module('yoPlannerAppResume', [])
 			$http.get("/montaje").success(function(data,status){
 				$scope.montajes = data;
 				$http.get("/rfp/"+queryDict.rfp).success(function(data,status){
-					console.log(data);
 					$scope.rfp=data;
 					$scope.rfp.sencillas = $scope.divideTipoHabitaciones(1,$scope.rfp.configuracionHabitaciones);
 					$scope.rfp.dobles = $scope.divideTipoHabitaciones(2,$scope.rfp.configuracionHabitaciones);
+					for(var i in $scope.rfp.salones){
+						$scope.rfp.salones[i].tipoEvento = $scope.findDescripcionTipoEvento($scope.rfp.salones[i].tipoEvento);
+						$scope.rfp.salones[i].tipoSalon = $scope.findDescripcionMontaje($scope.rfp.salones[i].tipoSalon);
+					}
 				});
 			});
 		});
 		$scope.formatDate= function(date){
 			return (moment(date).format("DD/MM"))
+		}
+		$scope.formatDateNormal= function(date){
+			return (moment(date).format("DD/MM/YYYY"))
+		}
+
+		$scope.findDescripcionTipoEvento= function(tipoEvento){
+			for(var i in $scope.tipoEventos){
+				if($scope.tipoEventos[i].id==tipoEvento){
+					return $scope.tipoEventos[i];
+				}
+			}
+			
+		}
+
+		$scope.findDescripcionMontaje= function(montaje){
+			for(var i in $scope.montajes){
+				if($scope.montajes[i].id==montaje){
+					return $scope.montajes[i];
+				}
+			}
 			
 		}
 	}]);
