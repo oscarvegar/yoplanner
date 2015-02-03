05dad0c8302e164b9711e6526856627bd012c12c
Oscar Vega <oscar.vega@ovegar.(none)>
2015-02-03 14:54:02 -0600

    auto pull on change

--- api/controllers/DeployController.js
+++ api/controllers/DeployController.js
@@ -4,10 +4,12 @@
  * @description :: Server-side logic for managing deploys
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-
+var exec = require('child_process').exec;
 module.exports = {
 	webhook:function(req,res){
-		console.log(">>>>> PUSH DETECTADO <<<<<");
+		exec('git pull', function (error, stdout, stderr) {
+		  console.log(">>>>> EXECUTING GIT PULL <<<<<")
+		});
 		res.send(200);
 	}
 };
