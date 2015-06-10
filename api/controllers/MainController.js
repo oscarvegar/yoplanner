cf0cd66280eb4ac7799f84fa9fc617128f305e2c
oscar.vega <oscar.vega@atomicware.mx>
2015-06-09 19:45:56 -0500

    initial yp v2

---
+++ api/controllers/MainController.js
@@ -0,0 +1,15 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side logic for managing Mains
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+	root:function(req,res){
+		res.view('homepage')
+	}
+	
+};
+
