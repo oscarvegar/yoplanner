cc09ba76a1e69532cbe510ef88fa05a3805a91d8
oscar.vega <oscar.vega@atomicware.mx>
2015-08-03 00:19:47 -0500

    linkedin share

--- assets/ng/controllers/BlogCtrl.js
+++ assets/ng/controllers/BlogCtrl.js
@@ -2,9 +2,29 @@ angular.module('yoplanner.blog',[])
 .controller('BlogCtrl',function($scope,$http,$sce,$filter,$stateParams,$state){
 	
 	$scope.init = function(){
+
 		$scope.current = $state.current;
 		console.log("STATE NAME >>>>>",$state.current.name)
-		if($state.current.name == 'blog'){
+		if($state.current.name == 'blogPost'){
+
+			$scope.posts = [];
+			$http.get('https://www.googleapis.com/blogger/v3/blogs/6838970422906123086/posts/'+$stateParams.id+'?key=AIzaSyBAg_S0Hde7VaxSVp_mmEB0gOdzCCO756Y')
+			.success(function(data){
+				var content = data.content;
+				var idxImgIni = content.search("<a href=");
+				var idxImgExt = content.search(".jpg");
+				if(idxImgExt<0)idxImgExt = content.search(".JPG");
+				if(idxImgExt<0)idxImgExt = content.search(".png");
+				if(idxImgExt<0)idxImgExt = content.search(".PNG");
+				if(idxImgExt<0)idxImgExt = content.search(".gif");
+				if(idxImgExt<0)idxImgExt = content.search(".GIF");
+				data.imagensrc = content.substr(idxImgIni+9,idxImgExt-idxImgIni-5);
+				$scope.posts.push(data)
+				
+			}).catch(function(err){
+				console.error(err)
+			})
+		}else{
 			$http.get('https://www.googleapis.com/blogger/v3/blogs/6838970422906123086/posts?key=AIzaSyBAg_S0Hde7VaxSVp_mmEB0gOdzCCO756Y')
 			.success(function(data){
 				console.log("BLOG DATA >>>>> ",data.items);
@@ -21,6 +41,7 @@ angular.module('yoplanner.blog',[])
 					post.contentText = post.content.substr(post.content.search("<a name='more'>"))
 					post.imagen = post.content.substr(0,post.content.search("<a name='more'>"))
 					post.limitText = $filter('limitTo')(post.contentText, 450);
+					post.limitText = post.limitText.replace("white","")
 					var content = post.content;
 					var idxImgIni = content.search("<a href=");
 					var idxImgExt = content.search(".jpg");
@@ -30,26 +51,17 @@ angular.module('yoplanner.blog',[])
 					if(idxImgExt<0)idxImgExt = content.search(".gif");
 					if(idxImgExt<0)idxImgExt = content.search(".GIF");
 					post.imagensrc = content.substr(idxImgIni+9,idxImgExt-idxImgIni-5);
+					if(!post.labels)post.labels=[];
+					post.resmume="";
+					for(var i in post.labels){
+						if(i==0)
+							post.resmume=""+post.labels[i];
+						else
+							post.resmume=post.resmume+", "+post.labels[i];
+					}
 				}
-			}).catch(function(err){
-				console.error(err)
-			})
-		}else if($state.current.name == 'blogPost'){
-
-			$scope.posts = [];
-			$http.get('https://www.googleapis.com/blogger/v3/blogs/6838970422906123086/posts/'+$stateParams.id+'?key=AIzaSyBAg_S0Hde7VaxSVp_mmEB0gOdzCCO756Y')
-			.success(function(data){
-				var content = data.content;
-				var idxImgIni = content.search("<a href=");
-				var idxImgExt = content.search(".jpg");
-				if(idxImgExt<0)idxImgExt = content.search(".JPG");
-				if(idxImgExt<0)idxImgExt = content.search(".png");
-				if(idxImgExt<0)idxImgExt = content.search(".PNG");
-				if(idxImgExt<0)idxImgExt = content.search(".gif");
-				if(idxImgExt<0)idxImgExt = content.search(".GIF");
-				data.imagensrc = content.substr(idxImgIni+9,idxImgExt-idxImgIni-5);
-				$scope.posts.push(data)
-				
+				$scope.latestPost = $scope.posts[0];
+				console.log("IMAGENSRC >>>>> "+$scope.latestPost.imagensrc)
 			}).catch(function(err){
 				console.error(err)
 			})
