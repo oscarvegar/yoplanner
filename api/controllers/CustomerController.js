94e594894db0ea996a3300ab6c625634555ecf72
Brandon Neri <arcaniteamp@gmail.com>
2016-10-28 11:25:32 -0500

    Fix restaurantes y widget

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -21,5 +21,16 @@ module.exports = {
 		Customer.create(data).then(function (customer) {
 			return res.json({message: 'Customer creado', customer: customer});
 		}).catch(console.log);
+	},
+
+	autocompleteCustomers: function (req, res) {
+		var id = req.user.id ? req.user.id : '';
+		var nombre = req.param('nombre');
+		Customer.find({user: id, nombreCliente: nombre}).then(function(data) {
+		  return res.json({customers: data});
+		}).catch(function(err) {
+			console.log(err);
+		  return res.json({customers: []});
+		});
 	}
 };
