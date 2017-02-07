305b0c8a3179b553fabeca531b197db45e4a6268
orvg <oscar.vega@gft.com>
2017-02-07 15:06:31 -0600

    se añade constants como template

--- config/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports.constants = {
-    
-    URL_PICTURES : "http://media.staticontent.com/media/pictures/",
-    CUSTOM_PICTURES : "http://localhost:1440/api/recinto/images/",
-    LOCAL_FILE_PICTURES : "./tmp/uploads/"
-}
